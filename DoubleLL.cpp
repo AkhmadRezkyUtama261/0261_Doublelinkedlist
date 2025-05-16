324bc854f026d16ceabebbefcb2f4e4b43af72e2
AkhmadRezkyUtama261 <ahkmadrezkyutama369@gmail.com>
2025-05-16 09:18:35 +0700

    Menambah Void hapus

--- DoubleLL.cpp
+++ DoubleLL.cpp
@@ -78,5 +78,21 @@ current->next->prev = newNode; // step 9c: current.next.prev = newNode
 current->next = newNode; //step 9d: current.next = newNode
 }
 
+void hapus()
+{
+    if (START == NULL)
+{
+    cout << "\nList is empty" << endl;
+    return;
+}
+
+cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+int rollNo;
+cin >> rollNo;
+
+Node *current = START;
+
+
+}
 
 }
