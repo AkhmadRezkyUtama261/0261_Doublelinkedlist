d244982f6871339b847564ba0ec7e0d04111f32a
AkhmadRezkyUtama261 <ahkmadrezkyutama369@gmail.com>
2025-05-16 09:06:44 +0700

    step 9 insert between current and current next

--- DoubleLL.cpp
+++ DoubleLL.cpp
@@ -55,6 +55,18 @@ if (START == NULL || nim <= START->noMhs)
     START = newNode;
     return;
 }
+// insert in between node
+//Step 8: Locate position for insertion
+Node *current = STAR;
+while (current->next != NULL && current->next->noMhs < nim)
+{
+    current = current->next;
+}
+if (current->next != NULL && nim == current->next->noMhs)
+{
+    cout << "\nDuplicate roll numbers not allowed" << endl;
+    return;
+}
 
 }
 }
